b9fcf87011b59cab5ff533b1f44004bcee4a07ab
John Carroll <13478618+john-carroll-sw@users.noreply.github.com>
2024-08-20 14:33:34 -0400

    updated the main.py for semantic kernel

--- semantic-kernel-python-webapi/main.py
+++ semantic-kernel-python-webapi/main.py
@@ -1,5 +1,6 @@
 import datetime
 import os
+import logging
 
 import dotenv
 from azure.core.exceptions import ClientAuthenticationError
@@ -7,8 +8,7 @@ from azure.identity import DefaultAzureCredential
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 from semantic_kernel import Kernel
-from semantic_kernel.connectors.ai.function_call_behavior import \
-    FunctionCallBehavior
+from semantic_kernel.connectors.ai.function_choice_behavior import FunctionChoiceBehavior
 from semantic_kernel.connectors.ai.open_ai.prompt_execution_settings.azure_chat_prompt_execution_settings import \
     AzureChatPromptExecutionSettings
 from semantic_kernel.connectors.ai.open_ai.services.azure_chat_completion import \
@@ -20,12 +20,17 @@ from semantic_kernel.exceptions.function_exceptions import \
     FunctionExecutionException
 from semantic_kernel.functions.kernel_arguments import KernelArguments
 
+# Configure logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
 dotenv.load_dotenv()
 
 app = FastAPI()
 
 pool_management_endpoint = os.getenv("POOL_MANAGEMENT_ENDPOINT")
 azure_openai_endpoint = os.getenv("AZURE_OPENAI_ENDPOINT")
+azure_deployment_name = os.getenv("AZURE_OPENAI_DEPLOYMENT")
 
 def auth_callback_factory(scope):
     auth_token = None
@@ -67,13 +72,13 @@ async def chat(message: str):
         service_id=service_id,
         ad_token_provider=auth_callback_factory("https://cognitiveservices.azure.com/.default"),
         endpoint=azure_openai_endpoint,
-        deployment_name="gpt-35-turbo",
+        deployment_name=azure_deployment_name,
     )
     kernel.add_service(chat_service)
 
     sessions_tool = SessionsPythonTool(
-        pool_management_endpoint,
         auth_callback=auth_callback_factory("https://dynamicsessions.io/.default"),
+        pool_management_endpoint=pool_management_endpoint,
     )
     kernel.add_plugin(sessions_tool, "SessionsTool")
 
@@ -85,12 +90,23 @@ async def chat(message: str):
 
     req_settings = AzureChatPromptExecutionSettings(service_id=service_id, tool_choice="auto")
 
-    filter = {"excluded_plugins": ["ChatBot"]}
-    req_settings.function_call_behavior = FunctionCallBehavior.EnableFunctions(auto_invoke=True, filters=filter)
+    req_settings.function_choice_behavior = FunctionChoiceBehavior.Auto(filters={"excluded_plugins": ["ChatBot"]})
 
     arguments = KernelArguments(settings=req_settings)
 
     history = ChatHistory()
+
+    system_message = """
+    You are a helpful AI assistant. 
+    You use your coding skill to solve problems. 
+    You have access to an IPython kernel to execute Python code. 
+    You output only valid python code. 
+    This valid code will be executed in a sandbox, resulting in result, stdout, or stderr. 
+    All necessary libraries have already been installed. 
+    Execute the code and return the result to the user.
+"""
+    
+    history.add_system_message(system_message)
 
     arguments["chat_history"] = history
     arguments["user_input"] = message
